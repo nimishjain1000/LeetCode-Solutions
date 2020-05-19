073c9b7d1e2b4b4df758d8f3ce2e97eec9c244a0
Nimish <nimishjain1000@gmail.com>
2020-05-19 21:55:01 +0530

    Create IsSymmetric.cpp

---
+++ BinaryTree/IsSymmetric.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSame(TreeNode* t1, TreeNode* t2){
+        
+        if(!t1 && !t2) return true;
+        if(t1 == NULL || t2 == NULL) return false;
+        
+        if(t1->val != t2->val) return false;
+        
+
+
+        return (isSame(t1->left, t2->right) && isSame(t1->right, t2->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        
+        if(!root) return true;
+        
+        return isSame(root->left, root->right);
+    }
+};
