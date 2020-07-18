afbf6bf6ca309e9c6dbb08e1b90744f92783e4c4
Nimish <nimishjain1000@gmail.com>
2020-07-18 23:45:55 +0530

    Create deleteANode.cpp
    Only steps.

---
+++ BinaryTree/deleteANode.cpp
@@ -0,0 +1,13 @@
+/*
+Delete a node from a Binary Tree
+
+*/
+
+
+/*
+Reach the last node ofthe tree ( level wise).
+Copy the data of that node in a variable and delete the node.
+Now search the key element which needs to be deleted.
+Reaching that element then copy the stored variable value in that element.
+
+*/
