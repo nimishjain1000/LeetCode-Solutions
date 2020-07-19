9100ce05a2dd20cb5e6ad22feb5aa2a9c0f8df07
Nimish <nimishjain1000@gmail.com>
2020-07-19 16:38:20 +0530

    Update IteratorOfBst.cpp

--- BST/IteratorOfBst.cpp
+++ BST/IteratorOfBst.cpp
@@ -53,4 +53,59 @@ public:
     }
 };
 
+/////////////////////////method 2////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+    stack<TreeNode*> st;
+public:
+    BSTIterator(TreeNode* root) {
+        
+        putLeftElements(root);
+    }
+    
+    void putLeftElements(TreeNode* root)
+    {
+        if(root == NULL) return;
+        
+        while(root!=NULL)
+        {
+            st.push(root);
+            root = root->left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        if(temp->right != NULL){
+            putLeftElements(temp->right);
+        }
+        return temp->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        if(st.size()!= 0) return true;
+        else return false;
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
  
